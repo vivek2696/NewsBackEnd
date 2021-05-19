7c575eaefb170c9d35fce195007faffb9c03d7ee
Vivek2696 <patelvivek1126@gmail.com>
2021-05-19 17:36:20 -0400

    Adding News CRUD Routes

--- routes/newsRoutes.js
+++ routes/newsRoutes.js
@@ -1,8 +1,170 @@
 const express = require("express");
+const newsModel = require("../models/newsModel");
 const router = express.Router();
 
-router.route("/news").get((req, res) => {
-    res.send("News API stuff");
-});
+//************* */ NEEDS SECRET KEY
+ 
+const NewsModel = require('../models/newsModel');
+
+router.get('/regular-news',(req, res) => {
+    NewsModel.find({ isSports : false }, (err, data)=> {
+        if(err){
+            console.log(err);
+            throw err;
+        }
+        else{
+            res.json(data);
+        }
+    })
+})
+
+router.get('/sports-news', (req, res) => {
+    NewsModel.find({ isSports : true }, (err, data) => {
+        if(err){
+            console.log(err);
+            throw err;
+        }
+        else{
+            res.json(data);
+        }
+    })
+})
+
+router.get('/admin/news', (req, res) => {
+    var headerInfo = req.headers.authorization;
+
+    if(headerInfo != null){
+        var token = headerInfo.replace('Bearer ', '');
+
+        try{
+            var result = jwt.verify(token, 'Secrete Key');
+
+            NewsModel.find((err, data) => {
+                if(err){
+                    console.log(err);
+                    throw err;
+                }
+                else{ 
+                    res.json(data);
+                }
+            });    
+        }catch(error){
+            res.status(500).json(error);
+        }
+    }
+    else{
+        console.log('No JWT!');
+        res.status(500).json({err: 'Not Authorized!!'});
+    }
+})
+
+router.post('/admin/news', (req, res) => {
+    var headerInfo = req.headers.authorization;
+    var newNews = new NewsModel();
+
+    newNews.title = req.body.title; 
+    newNews.description = req.body.description;
+    newNews.url = req.body.url;
+    newNews.image_url = req.body.image_url;
+    newNews.isSports = req.body.isSports;
+
+    if(headerInfo != null){
+        var token = headerInfo.replace('Bearer ', '');
+
+        try{
+            var result = jwt.verify(token, 'Secrete Key');
+
+            newNews.save((err, data) => {
+                if(err){
+                    console.log(err);
+                    throw err;
+                }
+                else{ 
+                    res.json(data);
+                }
+            });    
+        }catch(error){
+            res.status(500).json(error);
+        }
+    }
+    else{
+        console.log('No JWT!');
+        res.status(500).json({err: 'Not Authorized!!'});
+    }
+})
+
+
+router.put('/admin/news/:id', (req, res) => {
+    var headerInfo = req.headers.authorization;
+    var newsId = req.params.id;
+
+    if(headerInfo != null && !newsId){
+        var token = headerInfo.replace('Bearer ', '');
+
+        try{
+            var result = jwt.verify(token, 'Secrete Key');
+
+            NewsModel.findByIdAndUpdate(newsId, req.body, (err, data) => {
+                if(err){
+                    console.log(err);
+                    throw err;
+                }
+                else{ 
+                    res.json(data);
+                }
+            });    
+        }catch(error){
+            res.status(500).json(error);
+        }
+    }
+    else{
+        if(headerInfo == null){
+            console.log('No JWT');
+            res.status(500).json({err: 'Not Authorized!!'});
+        }
+        else{
+            console.log('No newId!');
+            res.status(500).json({err: 'NewsId not found!!'});
+        } 
+    }
+})
+
+router.delete('/admin/news/:id', (req, res) => {
+    var headerInfo = req.headers.authorization;
+    var newsId = req.params.id;
+
+    if(headerInfo != null && !newsId){
+        var token = headerInfo.replace('Bearer ', '');
+
+        try{
+            var result = jwt.verify(token, 'Secrete Key');
+
+            NewsModel.delete(newsId, (err, data) => {
+                if(err){
+                    console.log(err);
+                    throw err;
+                }
+                else{ 
+                    res.json(data);
+                }
+            });    
+        }catch(error){
+            res.status(500).json(error);
+        }
+    }
+    else{
+        if(headerInfo == null){
+            console.log('No JWT');
+            res.status(500).json({err: 'Not Authorized!!'});
+        }
+        else{
+            console.log('No newId!');
+            res.status(500).json({err: 'NewsId not found!!'});
+        } 
+    }
+})
+
+
+
 
 module.exports = router;
