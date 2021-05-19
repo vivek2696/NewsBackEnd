1dd5e963a42dbcd6cf4b432e34b19b6b948e7d9e
Francis K <frageokiz5@gmail.com>
2021-05-19 17:21:49 -0400

    some changes

--- index.js
+++ index.js
@@ -6,14 +6,24 @@ const app = express();
 
 app.use(express.json());
 
-const db = mongoose.connect("mongodb://127.0.0.1:27017/news");
+const db = mongoose.connect(
+    "mongodb://127.0.0.1:27017/news",
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    },
+    (err) =>
+        err
+            ? console.log("Something got wrong", err)
+            : console.log("DB Connected")
+);
 
 //Routes
 const adminRoutes = require("./routes/adminRoutes");
 const newsRoutes = require("./routes/newsRoutes");
 const queryRoutes = require("./routes/queryRoutes");
 
-app.use("/api", adminRoutes);
+app.use("/api/admin", adminRoutes);
 app.use("/api", newsRoutes);
 app.use("/api", queryRoutes);
 
--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,8 +1,22 @@
 const express = require("express");
 const router = express.Router();
 
-router.route("/admin").get((req, res) => {
+const Admin = require("../models/adminModel");
+
+router.route("/").get((req, res) => {
     res.send("Admin API stuff");
+});
+
+router.post("/login", (req, res) => {
+    const { email, password } = req.body;
+    Admin.findOne({ email }).then((account) => {
+        if (!account) {
+            res.status(404).json({ msg: "User Not found." });
+        } else {
+            res.send(account);
+        }
+    });
+    // Admin.create({ email, password });
 });
 
 module.exports = router;
