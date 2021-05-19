304fe249e8e07ceb84fcaae558a81e55173dfc34
Francis K <frageokiz5@gmail.com>
2021-05-19 19:39:44 -0400

    admin api

--- models/adminModel.js
+++ models/adminModel.js
@@ -1,5 +1,9 @@
 const mongoose = require("mongoose");
 Schema = mongoose.Schema;
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const SECRET_KEY = "NodeFinalProject@Team5SecreteKey";
 
 var adminModel = new Schema(
     {
@@ -11,6 +15,26 @@ var adminModel = new Schema(
         timestamps: true,
     }
 );
+
+adminModel.methods.genPasswordHash = function (inputPassword) {
+    const salt = bcrypt.genSaltSync(10);
+    this.password = bcrypt.hashSync(inputPassword, salt);
+};
+
+adminModel.methods.comparePasswordHash = function (inputPassword) {
+    return bcrypt.compareSync(inputPassword, this.password);
+};
+
+adminModel.methods.genUserObj = function () {
+    const payload = {
+        name: this.username,
+        email: this.email,
+    };
+
+    const token = jwt.sign(payload, SECRET_KEY);
+
+    return { token };
+};
 
 module.exports = mongoose.model("admin", adminModel, "AdminList");
 //the third argument is the collection name
--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -9,14 +9,48 @@ router.route("/").get((req, res) => {
 
 router.post("/login", (req, res) => {
     const { email, password } = req.body;
+    console.log(email + ": " + password);
     Admin.findOne({ email }).then((account) => {
+        // If account not exist, then 404
         if (!account) {
             res.status(404).json({ msg: "User Not found." });
-        } else {
-            res.send(account);
+        }
+        // Else compare password hash and set cookie with token
+        else {
+            console.log(account);
+
+            if (account.comparePasswordHash(password)) {
+                const token = account.genUserObj().token;
+                res.cookie("token", token, {
+                    maxAge: 900000,
+                    httpOnly: true,
+                });
+                console.log("Token:" + token);
+                res.send(token);
+            } else {
+                res.status(401).json({ msg: "Invalid Credentials." });
+            }
         }
     });
     // Admin.create({ email, password });
+});
+
+router.post("/register", (req, res) => {
+    const { name, email, password } = req.body;
+
+    const admin = new Admin();
+    admin.name = name;
+    admin.email = email;
+
+    admin.genPasswordHash(password);
+
+    admin
+        .save()
+        .then((newUser) => {
+            if (newUser) return res.json({ message: "Admin Created" });
+            else return res.json({ err: "Error creating Admin" });
+        })
+        .catch((err) => res.status(500).json(err));
 });
 
 module.exports = router;
