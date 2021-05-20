1f83d1bcae49eaeda40e674b0344747b61d9db64
Francis K <frageokiz5@gmail.com>
2021-05-20 18:13:03 -0400

    Added news url api

--- CustomerFacingNews/routes/newsRoutes.js
+++ CustomerFacingNews/routes/newsRoutes.js
@@ -1,49 +1,59 @@
 const express = require("express");
 const router = express.Router();
 
- 
-const NewsModel = require('../models/newsModel');
+const NewsModel = require("../models/newsModel");
 
-router.get('/regular-news',(req, res) => {
-    NewsModel.find({ isSports : false }, (err, data)=> {
-        if(err){
+router.get("/regular-news", (req, res) => {
+    NewsModel.find({ isSports: false }, (err, data) => {
+        if (err) {
             console.log(err);
             throw err;
-        }
-        else{
+        } else {
             console.log(data);
             res.json(data);
         }
-    })
-})
+    });
+});
 
-router.get('/sports-news', (req, res) => {
-    NewsModel.find({ isSports : true }, (err, data) => {
-        if(err){
+router.get("/sports-news", (req, res) => {
+    NewsModel.find({ isSports: true }, (err, data) => {
+        if (err) {
             console.log(err);
             throw err;
-        }
-        else{
+        } else {
             res.json(data);
         }
-    })
-})
+    });
+});
 
-router.get('/news/:id', (req, res) => {
+router.get("/news/:id", (req, res) => {
     let id = req.params.id;
-    console.log('Fetching news by id:',id);
+    console.log("Fetching news by id:", id);
 
-    NewsModel.findById(id,(err, data) => {
-        if(err){
+    NewsModel.findById(id, (err, data) => {
+        if (err) {
             console.log(err);
             res.status(500).json(err);
-        }
-        else{ 
+        } else {
             console.log(data);
             res.json(data);
         }
-    });    
-})
+    });
+});
 
+router.get("/news/url/:id", (req, res) => {
+    const newsURL = req.params.id;
+    console.log("Fetching news by URL:", newsURL);
+
+    NewsModel.findOne({ url: newsURL }, (err, data) => {
+        if (err) {
+            console.log(err);
+            res.status(500).json(err);
+        } else {
+            console.log(data);
+            res.json(data);
+        }
+    });
+});
 
 module.exports = router;
