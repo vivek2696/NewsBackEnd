3589d570d3e82f1f7bc1fe4673be8e06ba0a54eb
John Griffin <John@Johns-MacBook-Air-2.local>
2021-05-20 22:33:34 -0600

    Adding OpenWeather API page

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// hi
+const express = require("express");
+const mongoose = require("mongoose");
+
+const port = 3300;
+const app = express();
+
+app.use(express.json());
+
+const db = mongoose.connect("mongodb://127.0.0.1:27017/news");
+
+app.get("/", (req, res) => {
+    res.send("App is running!!");
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
