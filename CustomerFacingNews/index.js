86891ee40b56dd2210ec6d99b0a998e00459eda5
Francis K <frageokiz5@gmail.com>
2021-05-20 21:38:58 -0400

    Add socket io backend

--- CustomerFacingNews/index.js
+++ CustomerFacingNews/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 ////////////////////////////////////////////////
 const path = require("path");
@@ -12,7 +13,18 @@ const publicIp = require("public-ip");
 ////////////////////////////////////////////////
 const port = 3400;
 const app = express();
+const httpServer = http.createServer(app);
+// const { Server } = require("socket.io");
+// const io = new Server(server);
 
+const io = require("socket.io")(httpServer, {
+    cors: {
+        origin: "http://localhost:4200",
+        methods: ["GET", "POST"],
+    },
+});
+
+app.use(cors());
 app.use(express.json());
 
 /////////////////////////////////
@@ -20,13 +32,15 @@ app.set(port);
 app.use(express.static(path.join(__dirname, "public")));
 /////////////////////////////////
 const db = mongoose.connect(
-  "mongodb://127.0.0.1:27017/News",
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  },
-  (err) =>
-    err ? console.log("Something got wrong", err) : console.log("DB Connected")
+    "mongodb://127.0.0.1:27017/News",
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    },
+    (err) =>
+        err
+            ? console.log("Something got wrong", err)
+            : console.log("DB Connected")
 );
 
 //Routes
@@ -37,42 +51,53 @@ app.use("/api", newsRoutes);
 app.use("/api", queryRoutes);
 
 app.get("/", (req, res) => {
-  res.send("App is running!!");
+    res.send("App is running!!");
 });
 
-let server = app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+httpServer.listen(port, () => {
+    console.log("listening on *:" + port);
 });
-
-// Set up socket.io
-const socketi = require("socket.io");
-const io = socketi.listen(server);
 
 // Handle socket traffic
 io.sockets.on("connection", (socket) => {
-  var list = io.sockets.sockets;
-  var users = Object.keys(list);
+    socket.myCustomStorage = [];
+    var list = io.sockets.sockets;
+    var users = Object.keys(list);
 
-  // Set the name property for a given client
-  socket.on("nick", (nick) => {
-    socket.set("nickname", nick);
-    socket.emit("userlist", users);
-    socket.emit("datetime", { datetime: new Date().getTime() });
-  });
-
-  // Relay chat data to all clients
-  socket.on("chat", (data) => {
-    socket.get("nickname", (err, nick) => {
-      let nickname = err ? "Anonymous" : nick;
-
-      let payload = {
-        message: data.message,
-        nick: nickname,
-        time: new Date().toLocaleTimeString("en-US"),
-      };
-
-      socket.emit("chat", payload);
-      socket.broadcast.emit("chat", payload);
+    // Set the name property for a given client
+    socket.on("nick", (nick) => {
+        socket.myCustomStorage["nickname"] = nick;
+        //socket.set("nickname", nick);
+        socket.emit("userlist", users);
+        socket.emit("datetime", { datetime: new Date().getTime() });
     });
-  });
+
+    // Relay chat data to all clients
+    socket.on("chat", (data) => {
+        let nickname = socket.myCustomStorage["nickname"]
+            ? socket.myCustomStorage["nickname"]
+            : "Anonymous";
+
+        let payload = {
+            message: data.message,
+            nick: nickname,
+            time: new Date().toLocaleTimeString("en-US"),
+        };
+
+        socket.emit("chat", payload);
+        socket.broadcast.emit("chat", payload);
+
+        // socket.get("nickname", (err, nick) => {
+        //     let nickname = err ? "Anonymous" : nick;
+
+        //     let payload = {
+        //         message: data.message,
+        //         nick: nickname,
+        //         time: new Date().toLocaleTimeString("en-US"),
+        //     };
+
+        //     socket.emit("chat", payload);
+        //     socket.broadcast.emit("chat", payload);
+        // });
+    });
 });
